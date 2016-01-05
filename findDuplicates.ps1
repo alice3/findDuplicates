c7b61c3c3680d633258dbc5d79552c8d99eadbb2
Alice <alice@example.net>
2016-01-05 17:55:24 +0000

    Initial commit
    Added the script

---
+++ findDuplicates.ps1
@@ -0,0 +1,15 @@
+﻿function findDuplicateFiles {
+    Param([System.IO.DirectoryInfo]$Directory = ".")
+    [array]$files = Get-ChildItem -Recurse $Directory
+    [System.Collections.ArrayList]$return = @()
+
+    for($i = 0; $i -lt $files.Count; $i++){
+        for($j = $i + 1; $j -lt $files.Count; $j++){
+            Write-Debug "Comparing $($files[$i].FullName) and $($files[$j].FullName)"
+            if( -not (Compare-Object (Get-Content $files[$i].FullName) (Get-Content $files[$j].FullName))) {
+                $return.Add(@($files[$i],$files[$j])) | Out-Null
+            }
+        }
+    }
+    return $return
+}
